6e2266d694d24ec5ec0f96d6e4181f441329b524
Wojciech Zarski <wojciech.zarski@gmail.com>
2016-12-29 16:50:26 +0100

    Add wifi manager support wifi reset on GPIO2 FALLING state

--- esp-program/src/server.ino
+++ esp-program/src/server.ino
@@ -1,94 +1,58 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 
-const char* ssid = "*******";
-const char* password = "*******";
-MDNSResponder mdns;
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 ESP8266WebServer server(80);
+const  int led = 13;
+const int buttonPin = 2;
+int state = LOW;
 
-const int led = 13;
+WiFiManager wifiManager;
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from server!");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound(){
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
-
-void handleRC() { //Handler
-  String message = “Number of args received:”;
-  message += server.args();            //Get number of parameters
-  message += “\n”;                            //Add a new line
-
-  for (int i = 0; i < server.args(); i++) {
-
-  message += “Arg nº” + (String)i + ” –> “;   //Include the current iteration value
-  message += server.argName(i) + “: “;     //Get the name of the parameter
-  message += server.arg(i) + “\n”;              //Get the value of the parameter
-
-  }
-
-  server.send(200, “text/plain”, message);       //Response to the HTTP request
-}
-
-
-void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+void setup() {
+  // put your setup code here, to run once:
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin),reset, FALLING);
+
+  wifiManager.autoConnect("SmartPowerAP");
+
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-  }
-
   server.on("/", handleRoot);
-
-  server.on("/api/rc", handleRC);
-
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
 }
 
+void reset() {
+  Serial.println("I'm resetting my settings!");
+  delay(1000); //debounce
+  wifiManager.resetSettings();
+  Serial.println("I'm going to reset");
+  ESP.reset();
+  Serial.println("End of interruption!");
+}
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!");
+  digitalWrite(led, 0);
+}
+
+
+
 void loop(void){
   server.handleClient();
+
 }
